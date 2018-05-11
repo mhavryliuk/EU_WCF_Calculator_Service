aa0d4f560d92ac1191913b7281b1e82fb681008a
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-05-11 23:36:16 +0300

    WCF: Implementation of the calculator (service).

---
+++ CalcService.svc.cs
@@ -0,0 +1,71 @@
+﻿namespace _20180412_WCF_Calculator_Service
+{
+    public class CalcService : ICalculator
+    {
+        #region Common Methods
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Проверка соединения.
+        /// </summary>
+        /// <returns>OK</returns>
+        public string TestConnection()
+        {
+            return "OK";
+        }
+
+        #endregion
+
+        #region Arithmetic
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Метод AddNumber() складывает два числа.
+        /// </summary>
+        /// <param name="number1">Слагаемое 1.</param>
+        /// <param name="number2">Слагаемое 2.</param>
+        /// <returns>Сумма двух чисел.</returns>
+        public double AddNumber(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Метод SubNumber() вычитает второе число из первого.
+        /// </summary>
+        /// <param name="number1">Уменьшаемое число.</param>
+        /// <param name="number2">Вычитаемое число.</param>
+        /// <returns>Разность.</returns>
+        public double SubNumber(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Метод MultNumber() умножает два числа.
+        /// </summary>
+        /// <param name="number1">Множитель 1.</param>
+        /// <param name="number2">Множитель 2.</param>
+        /// <returns>Произведение двух чисел.</returns>
+        public double MultNumber(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Метод DivNumber() делит первое число на второе.
+        /// </summary>
+        /// <param name="number1">Делимое.</param>
+        /// <param name="number2">Делитель.</param>
+        /// <returns>Частное.</returns>
+        public double DivNumber(double number1, double number2)
+        {
+            return number1 / number2;
+        }
+
+        #endregion
+    }
+}
---
+++ ICalculator.cs
@@ -0,0 +1,59 @@
+﻿using System.ServiceModel;
+
+namespace _20180412_WCF_Calculator_Service
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        #region Common Methods
+
+        /// <summary>
+        /// Проверка соединения.
+        /// </summary>
+        /// <returns>OK</returns>
+        [OperationContract]
+        string TestConnection();
+
+        #endregion
+
+        #region Arithmetic
+
+        /// <summary>
+        /// Метод AddNumber() складывает два числа.
+        /// </summary>
+        /// <param name="number1">Слагаемое 1.</param>
+        /// <param name="number2">Слагаемое 2.</param>
+        /// <returns>Сумма двух чисел.</returns>
+        [OperationContract]
+        double AddNumber(double number1, double number2);
+
+        /// <summary>
+        /// Метод SubNumber() вычитает второе число из первого.
+        /// </summary>
+        /// <param name="number1">Уменьшаемое число.</param>
+        /// <param name="number2">Вычитаемое число.</param>
+        /// <returns>Разность.</returns>
+        [OperationContract]
+        double SubNumber(double number1, double number2);
+
+        /// <summary>
+        /// Метод MultNumber() умножает два числа.
+        /// </summary>
+        /// <param name="number1">Множитель 1.</param>
+        /// <param name="number2">Множитель 2.</param>
+        /// <returns>Произведение двух чисел.</returns>
+        [OperationContract]
+        double MultNumber(double number1, double number2);
+
+        /// <summary>
+        /// Метод DivNumber() делит первое число на второе.
+        /// </summary>
+        /// <param name="number1">Делимое.</param>
+        /// <param name="number2">Делитель.</param>
+        /// <returns>Частное.</returns>
+        [OperationContract]
+        double DivNumber(double number1, double number2);
+
+        #endregion
+    }
+}
